b4dfa307805eb8d917a47ae5220e0a5900035b2a
Chris Collier <possiblellama@gmail.com>
2019-03-03 22:01:53 +0000

    Moved mongo properties out into database properties file (ignored), and pulled into spring via config class.

---
+++ api/plotpoints/src/main/java/org/kaccag/AppConfig.java
@@ -0,0 +1,9 @@
+package org.kaccag;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("database.properties")
+public class AppConfig {
+}
