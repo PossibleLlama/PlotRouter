91a9959b379b9c4e5a34447ccf4a70e3202fc01a
llama <possiblellama@gmail.com>
2019-03-12 23:03:46 +0000

    Added basic bubble menu item

---
+++ web/src/components/BubbleMenu.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="circle">{{ msg }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BubbleMenu extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+.circle
+    {
+    border-radius:50%;
+    padding-top: 1em;
+    text-align:center;
+    font-size: medium;
+    color:rgb(231, 241, 227);
+    background:rgb(56, 97, 54);
+    }
+</style>
+
