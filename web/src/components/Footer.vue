d713640784ff220381a7387ff45c5fc875473c34
llama <possiblellama@gmail.com>
2019-03-16 19:08:28 +0000

    Moved vue app to base of body, and header and footer into their own components

---
+++ web/src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <div class="content container-fluid">
+      <div class="row">
+        <div class="col-sm-8">
+          <!-- TODO create footer -->
+          <p>Created by PossibleLlama</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
