808505850ceecb809e5acd7e30af9bbe512ec6c1
llama <possiblellama@gmail.com>
2019-03-17 13:43:09 +0000

    Altering chai tests to look for elements within the component

---
+++ web/tests/unit/header.spec.ts
@@ -0,0 +1,17 @@
+import Header from '@/components/Header.vue';
+import { mount } from '@vue/test-utils';
+import { expect } from 'chai';
+
+describe('Component has elements', () => {
+  it('Components has a header', async () => {
+    const wrapper = mount(Header);
+
+    expect(wrapper.contains('header')).to.equal(true);
+  });
+
+  it('Components has navigation bar', async () => {
+    const wrapper = mount(Header);
+
+    expect(wrapper.contains('nav')).to.equal(true);
+  });
+});
