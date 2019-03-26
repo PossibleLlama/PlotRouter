841c42dccd80d76ba21bd4462e72b2266e8ff299
llama <possiblellama@gmail.com>
2019-03-26 22:32:45 +0000

    Added basis of overlay

--- web/src/components/navbar/Session.vue
+++ web/src/components/navbar/Session.vue
@@ -2,6 +2,9 @@
     <div class="session">
         <button class="btn" v-if="loggedIn" @click="toggleSessionStatus">Logout</button>
         <button class="btn" v-else @click="toggleSessionStatus">Login</button>
+        
+        <div id="overlay" v-if="overlay">
+        </div>
     </div> <!-- Session -->
 </template>
 
@@ -13,10 +16,12 @@ import { Component, Vue } from 'vue-property-decorator';
   },
 })
 export default class Session extends Vue {
+  private 'overlay': boolean = false;
   private 'loggedIn': boolean = false;
   private 'username': string = 'Username';
 
   private 'toggleSessionStatus'(): void {
+    this.overlay = true;
     this.loggedIn = !this.loggedIn;
     this.$emit('clicked', this.username, this.loggedIn);
   }
@@ -24,5 +29,17 @@ export default class Session extends Vue {
 </script>
 
 <style>
+#overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5);
+  z-index: 2;
+  cursor: pointer;
+}
 </style>
 
