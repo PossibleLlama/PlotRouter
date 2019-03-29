59d60ddd05f347f05b750d1423904bbf5bb60bc5
llama <possiblellama@gmail.com>
2019-03-29 15:16:39 +0000

    Moving overlay out into seperate file

---
+++ web/src/components/Overlay.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="overlay" v-if="overlay">
+        <div id="overlayBox">
+            <button id="closeOverlayBtn" class="btn" @click="setOverlay(false)">X</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class Overlay extends Vue {
+  private 'overlay': boolean = false;
+
+  public 'setOverlay'(desiredState: boolean): void {
+    this.overlay = desiredState;
+  }
+}
+</script>
+
+<style>
+#overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5);
+  z-index: 2;
+  cursor: pointer;
+}
+
+#overlayBox {
+  margin: auto;
+  width: 80%;
+  height: 80%;
+  margin-top: 5%;
+  background-color: rgb(230,230,230);
+}
+
+#closeOverlayBtn {
+  position: relative;
+  left: 97%;
+}
+</style>
