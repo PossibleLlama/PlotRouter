8afaced476a8150d69d371e67af9be741130d6b4
llama <possiblellama@gmail.com>
2019-03-29 15:16:39 +0000

    Moving overlay out into seperate file

--- web/src/components/navbar/Session.vue
+++ web/src/components/navbar/Session.vue
@@ -1,45 +1,42 @@
 <template>
     <div class="session">
-        <button class="btn" v-if="loggedIn" @click="toggleSessionStatus">Logout</button>
-        <button class="btn" v-else @click="toggleSessionStatus">Login</button>
+        <button class="btn" v-if="!loggedIn" @click="openOverlay">Login</button>
+        <button class="btn" v-else @click="logout">Logout</button>
         
-        <div id="overlay" v-if="overlay">
-        </div>
+        <Overlay ref="childOverlay">
+
+        </Overlay>
     </div> <!-- Session -->
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Overlay from '../Overlay.vue';
 
 @Component({
   components: {
+    Overlay,
   },
 })
 export default class Session extends Vue {
-  private 'overlay': boolean = false;
   private 'loggedIn': boolean = false;
-  private 'username': string = 'Username';
 
-  private 'toggleSessionStatus'(): void {
-    this.overlay = true;
-    this.loggedIn = !this.loggedIn;
-    this.$emit('clicked', this.username, this.loggedIn);
+  private 'login'(user: string): void {
+    this.loggedIn = true;
+    this.$emit('clicked', user, true);
+  }
+
+  private 'logout'(user: string): void {
+    this.loggedIn = false;
+    this.$emit('clicked', user, false);
+  }
+
+  private 'openOverlay'(): void {
+    this.$refs.childOverlay.setOverlay(true);
   }
 }
 </script>
 
 <style>
-#overlay {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0,0,0,0.5);
-  z-index: 2;
-  cursor: pointer;
-}
 </style>
 
