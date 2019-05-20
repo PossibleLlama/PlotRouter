2d6a9f0d6aa22c5b7acba0ce2f572e2f0d08965a
llama <possiblellama@gmail.com>
2019-05-20 22:52:39 +0100

    Linter prefers equal(true) :(

--- web/tests/unit/header.spec.ts
+++ web/tests/unit/header.spec.ts
@@ -6,21 +6,21 @@ describe('Component has elements', () => {
   it('Component has a header', async () => {
     const wrapper = mount(Header);
 
-    expect(wrapper.contains('header')).to.be.true;
+    expect(wrapper.contains('header')).to.equal(true);
   });
 
   it('Header has navigation bar', async () => {
     const wrapper = mount(Header);
 
-    expect(wrapper.contains('nav')).to.be.true;
-    expect(wrapper.contains('header > nav')).to.be.true;
+    expect(wrapper.contains('nav')).to.equal(true);
+    expect(wrapper.contains('header > nav')).to.equal(true);
   });
 
   it('Header has link to home', async () => {
     const wrapper = mount(Header);
 
-    expect(wrapper.contains('a')).to.be.true;
-    expect(wrapper.contains('a > h1')).to.be.true;
+    expect(wrapper.contains('a')).to.equal(true);
+    expect(wrapper.contains('a > h1')).to.equal(true);
     expect(wrapper.find('a > h1').text()).to.equal('PlotRouter');
   });
 });
@@ -37,6 +37,6 @@ describe('Headers events emitted and received', () => {
 
     // No idea why this has nested arrays
     expect(wrapper.emitted('clicked')[0][0].username).to.equal(testUsername);
-    expect(wrapper.emitted('clicked')[0][0].loggedIn).to.be.true;
+    expect(wrapper.emitted('clicked')[0][0].loggedIn).to.equal(true);
   });
 });
