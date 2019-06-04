a6ed7e41e1f12ace61a673beba9d946dcbaa68d6
llama <possiblellama@gmail.com>
2019-06-04 23:01:09 +0100

    016 Basis of getting list of plot points

--- web/src/components/PlotPointList.vue
+++ web/src/components/PlotPointList.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script lang="ts">
+import axios from 'axios';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import PlotPointBox from './PlotPointBox.vue';
 
@@ -16,10 +17,30 @@ import PlotPointBox from './PlotPointBox.vue';
 export default class PlotPointList extends Vue {
     @Prop(String) public readonly user!: string;
 
-    private mounted(): void {
-        // get host name for endpoints
-        // make request to '{{plotpoint}}/user/{{user}}'
-        // v-for each object in data.plotPoints, create a PlotPointBox object
+    public plotPointsList: Array<{
+      id: string,
+      user: string,
+      summary: string,
+      description: string,
+      preceedingId: string,
+    }> = [];
+    private baseUrl: string = 'http://localhost:8000/api/plotpoint';
+
+    private async mounted(): Promise<void> {
+      const plotPointsList = await this.getAllPlotPointsForUser(this.user);
+      // v-for each object in data.plotPoints, create a PlotPointBox object
+    }
+
+    private async getAllPlotPointsForUser(user: string): Promise<Array<{
+      id: string,
+      user: string,
+      summary: string,
+      description: string,
+      preceedingId: string,
+    }>> {
+      const getAllByUserUrl = `${this.baseUrl}/user/${user}`;
+      const apiResponse = await axios.get(getAllByUserUrl);
+      return apiResponse.data.plotPoints;
     }
 }
 </script>
