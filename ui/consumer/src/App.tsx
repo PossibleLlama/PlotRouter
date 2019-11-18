4394e5ae6fbc63b3968f36ed435e72cade36ced2
Chris Collier <possiblellama@gmail.com>
2019-11-18 00:49:11 +0000

    025 Attempted bootstrap

--- ui/consumer/src/App.tsx
+++ ui/consumer/src/App.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <nav role="navigation">
-              <a
-                className="App-link"
-                href="#"
-              ><h1>PlotRouter</h1></a>
-              <a
-                className="App-link"
-                href="#"
-              ><h2>Events</h2></a>
-        </nav>
+          <Navbar collapseOnSelect sticky="top" expand="lg" role="navigation">
+            <Navbar.Brand href="#home" >Plot Router</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#Events">Events</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
 
       <footer>
         <p>Created by PossibleLlama</p>
       </footer>
-    </div>
+    </div> // App
   );
 }
 
